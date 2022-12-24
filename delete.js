b070112f3fca0d2800b45f7530d7904ddc4d842d
Gabriel Rodrigues <dev.gabrielrs@gmail.com>
2022-12-24 08:43:32 -0300

    add files

---
+++ delete.js
@@ -0,0 +1,9 @@
+// Deleta uma propriedade de um obj
+
+const pessoa ={
+    name: 'Gabriel',
+    idade: 20
+}
+delete pessoa.idade;
+
+console.log(pessoa);
---
+++ ternario.js
@@ -0,0 +1,11 @@
+// Condição então valor 1 se não valor 2
+
+let cafe_da_manha;
+let pao = true;
+let arroz = false;
+
+cafe_da_manha = pao || arroz ? 'é café da manha' : 'Não é café da manha';
+
+console.log(cafe_da_manha)
+
+
